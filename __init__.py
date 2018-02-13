f4338b128ac7932136a4e6a524c6b368d4330826
jongyoonl <jylee1307@gmail.com>
2018-02-13 18:45:04 -0500

    added pydocs

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""
+This package is a suite for finding the longest word on a Bookworm board.
+tree.py provides a data structure for representing the dictionary in a tree form.
+wordtree_maker.py parses the words file to create the tree.
+bookworm_solver.py uses the tree created by wordtree_maker.py to find the
+longest word possible on the board.
+unittests.py contains all unit tests for the modules in this package.
+"""
